ecb6b378173c9a952c077a33b2b388a94e988f64
PCNhat <nhat.pcn@gmail.com>
2025-02-06 15:22:51 +0700

    [nvim][neotree]Open with System Viewer

--- nvim/lua/nhatpc/plugins/neotree.lua
+++ nvim/lua/nhatpc/plugins/neotree.lua
@@ -2,18 +2,29 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
     dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim",
+        -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     },
-    config = function ()
-        local stats = vim.uv.fs_stat(vim.fn.argv(0))
-        if stats and stats.type == "directory" then
-          -- vim.cmd("Neotree filesystem reveal left")
-          --vim.cmd("Neotree position=current")
-        end
+    config = function()
+        require("neo-tree").setup({
+            filesystem = {
+                window = {
+                    mappings = {
+                        ["o"] = "system_open",
+                    },
+                },
+            },
+            commands = {
+                system_open = function(state)
+                    local node = state.tree:get_node()
+                    local path = node:get_id()
+                    vim.fn.jobstart({ "xdg-open", path }, { detach = true })
+                end,
+            },
+        })
 
         vim.keymap.set("n", "<leader>e", ":Neotree left reveal<CR>", { desc = "File Explorer", silent = true })
-    end
+    end,
 }
