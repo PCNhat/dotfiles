8c062af8deb91d4a5deb5dd327b9d3ac9656c356
PCNhat <nhat.pcn@gmail.com>
2025-02-11 17:29:38 +0700

    [nvim][neogen]Using luasnip engine to control the annotation

--- nvim/lua/nhatpc/plugins/neogen.lua
+++ nvim/lua/nhatpc/plugins/neogen.lua
@@ -4,9 +4,7 @@ return {
     config = function ()
         local neogen = require("neogen")
         vim.keymap.set("n", "<Leader>ng", neogen.generate, { desc = "[N]eogen [G]enerate" })
-        vim.keymap.set('i', '<C-l>', neogen.jump_next, { desc = "Neogen jump next" })
-        vim.keymap.set('i', '<C-h>', neogen.jump_prev, { desc = "Neogen jump previous" })
 
-        neogen.setup()
+        neogen.setup({ snippet_engine = "luasnip" })
     end
 }
