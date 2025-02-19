ac2c1f7b444e128dbfdad4249a70ddbac541f936
PCNhat <nhat.pcn@gmail.com>
2025-02-19 15:52:48 +0700

    [nvim][completion]Add custom snippets

--- nvim/lua/nhatpc/plugins/completion.lua
+++ nvim/lua/nhatpc/plugins/completion.lua
@@ -11,18 +11,24 @@ return {
 
         config = function ()
             require("luasnip.loaders.from_vscode").lazy_load()
+            require("luasnip.loaders.from_vscode").load({ paths = "~/.config/nvim/custom_snips" })
             require("luasnip").filetype_extend("htmldjango", { "html" })
-            local ls = require('luasnip')
-            vim.keymap.set({"i", "s"}, "<C-l>", function ()
+            local ls = require("luasnip")
+            vim.keymap.set({ "i", "s" }, "<C-l>", function()
                 if ls.expand_or_jumpable() then
                     ls.expand_or_jump()
                 end
-            end, {silent = true})
-            vim.keymap.set({"i", "s"}, "<C-h>", function ()
+            end, { silent = true })
+            vim.keymap.set({ "i", "s" }, "<C-h>", function()
                 if ls.jumpable(-1) then
                     ls.jump(-1)
                 end
-            end, {silent = true})
+            end, { silent = true })
+            vim.keymap.set({ "i", "s" }, "<C-e>", function()
+                if ls.choice_active() then
+                    ls.change_choice(1)
+                end
+            end, { silent = true })
         end
     },
     {
