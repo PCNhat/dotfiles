5a22ac277b4ebadc2147fe7059efdbcc8493742f
PCNhat <nhat.pcn@gmail.com>
2025-06-05 16:47:52 +0000

    [nvim][lsp]Disable vuels when working on Vue3 and Typescript projects

--- nvim/lua/nhatpc/plugins/lsp.lua
+++ nvim/lua/nhatpc/plugins/lsp.lua
@@ -46,6 +46,24 @@ return {
                             }
                         }
                     end,
+
+                    ["vuels"] = function ()
+                        local util = require("lspconfig.util")
+                        local function has_tsconfig(root_dir)
+                            return vim.fn.filereadable(root_dir .. "/tsconfig.json") == 1
+                        end
+
+                        local lspconfig = require("lspconfig")
+                        lspconfig.vuels.setup {
+                            root_dir = function (fname)
+                                local root = util.root_pattern("package.json", ".git")(fname)
+                                if root and not has_tsconfig(root) then
+                                    return root
+                                end
+                                return nil
+                            end
+                        }
+                    end
                 }
             })
 
