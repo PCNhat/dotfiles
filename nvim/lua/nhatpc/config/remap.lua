3e0ed286734e38da8c5b7c6ab66787ee65958f07
PCNhat <nhat.pcn@gmail.com>
2025-08-25 14:08:13 +0700

    [nvim][remap] Remove keybinds

--- nvim/lua/nhatpc/config/remap.lua
+++ nvim/lua/nhatpc/config/remap.lua
@@ -1,14 +1,5 @@
 vim.g.mapleader = " "
 
--- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
---
---  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
