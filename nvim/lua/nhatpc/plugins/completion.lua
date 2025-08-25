73af9939431f09ca0a73c2f8922b31e0160ec3ff
PCNhat <nhat.pcn@gmail.com>
2025-08-25 14:22:01 +0700

    [nvim][remap] Remove keybinds

--- nvim/lua/nhatpc/plugins/completion.lua
+++ nvim/lua/nhatpc/plugins/completion.lua
@@ -26,11 +26,11 @@ return {
                     ls.expand_or_jump()
                 end
             end, { silent = true })
-            vim.keymap.set({ "i", "s" }, "<C-h>", function()
-                if ls.jumpable(-1) then
-                    ls.jump(-1)
-                end
-            end, { silent = true })
+            -- vim.keymap.set({ "i", "s" }, "<C-h>", function()
+            --     if ls.jumpable(-1) then
+            --         ls.jump(-1)
+            --     end
+            -- end, { silent = true })
             vim.keymap.set({ "i", "s" }, "<C-e>", function()
                 if ls.choice_active() then
                     ls.change_choice(1)
